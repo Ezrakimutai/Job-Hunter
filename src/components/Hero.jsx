94e417d0f402c00854c87572ae485d77dba3b7f0
kimtaiezra <kimtaiezra02@gmail.com>
2025-06-19 16:00:16 +0300

    Fix: Updated Hero section to accept default props.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Hero = ({ title="Fintech", subtitle="Make Payment Systems Great Again!!" }) => {
+const Hero = ({ title="Fintech", subtitle="Make Job Hunting Systems Great Again!!" }) => {
   return (
     <section className="bg-indigo-700 py-20 mb-4">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center">
