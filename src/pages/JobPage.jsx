e90efec180eb4cfcfba6246307442550b4f13304
kimtaiezra <kimtaiezra02@gmail.com>
2025-06-19 16:10:46 +0300

    Removed redundant code

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -119,20 +119,4 @@ const jobLoader = async ({ params }) => {
 
 export { JobPage as default, jobLoader };
 
-// const [job, setJob] = useState(null);
-// const [loading, setLoading] = useState(true);
 
-// useEffect(() => {
-//   const fetchJob = async () => {
-//     try {
-//       const res = await fetch(`/api/jobs/${id}`);
-//       const data = await res.json();
-//       setJob(data);
-//     } catch (error) {
-//       console.log("Error fetching data", error);
-//     } finally {
-//       setLoading(false);
-//     }
-//   };
-//   fetchJob();
-// }, []);
